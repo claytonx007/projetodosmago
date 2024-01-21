ef51ebe64fa4e6eeb7334dcf5fd56ed819a5e0bf
claytonx007 <deproprio@gmail.com>
2024-01-21 19:28:37 -0300

    final

--- funcoesdodia.js
+++ funcoesdodia.js
@@ -1,9 +1,22 @@
-function calcular (saldo){
-    if (saldo <=10){
-    return "ferro"
+function calcular(saldo) {
+    if (saldo <= 10) {
+        return "ferro";
+    } else if (saldo <= 20) {
+        return "bronze";
+    } else if (saldo <= 50) {
+        return "prata";
+    } else if (saldo <= 80) {
+        return "ouro";
+    } else if (saldo <= 90) {
+        return "diamante";
+    } else if (saldo <= 100) {
+        return "lendario";
+    } else if (saldo >= 101) {
+        return "diamante";
     }
-} 
+}
 
-let resultado = calcular(9);
-console.log (resultado)
-console.log (calcular)
+// Exemplo de uso
+let saldo = 30;
+let nivel = calcular(saldo);
+console.log("Você tem " + saldo + " vitórias e está no Nível " + nivel);
